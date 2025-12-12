4c16f48332a5e02c6db6879a7beff83435cc8e24
deepdevops11 <deepdevops11@gmail.com>
2025-12-12 18:32:40 +0530

    made changes in name

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "abhishek" {
-  instance_type = "t2.micro"
+  instance_type = "t2.micro" #change this
   ami = "ami-053b0d53c279acc90" # change this
   subnet_id = "subnet-019ea91ed9b5252e7" # change this
 }

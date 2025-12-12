40df179af6054d9aa671c7de4bc57486267683d7
Deepanshu Deep Pandey <deepdevops11@gmail.com>
2025-12-12 19:56:07 +0530

    Update main.tf

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "Deepanshu" {
+resource "aws_instance" "Deepanshu Deep Pandey" {
   instance_type = "t2.micro" #change this
   ami = "ami-053b0d53c279acc90" # change this
   subnet_id = "subnet-019ea91ed9b5252e7" # change this
